1f61f1d50bc3684d099ce2de07ace1697df1d03a
Yogesh <yogesh.204081@sggscc.ac.in>
2022-01-04 17:39:04 +0530

    Forms and posts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import Posts from './components/Posts/Posts'
+import Form from './components/Forms/Form'
+import {Container, AppBar, Typography, Grow, Grid} from "@material-ui/core"
+import Instaverse from './images/Instaverse.png'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Instaverse</Typography>
+        <img src={Instaverse} alt="instaverse" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems='stretch' spacing="4">
+            <Grid item xs={12} sm={7}>
+              <Posts/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
