0e5d2db88ed5aa0b628c54b51c956c6b0a6db06d
Yogesh <yogesh.204081@sggscc.ac.in>
2022-01-09 17:15:03 +0530

    posting our posts

--- src/App.js
+++ src/App.js
@@ -18,13 +18,13 @@ const App = () => {
 
   return (
     <Container maxWidth="lg">
-      <AppBar classname={classes.appBar} position="static" color="inherit">
+      <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">Instaverse</Typography>
         <img className={classes.image} src={Instaverse} alt="instaverse" height="60" />
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justify="space-between" alignItems='stretch' spacing="4">
+          <Grid container justifyContent="space-between" alignItems='stretch' spacing={4}>
             <Grid item xs={12} sm={7}>
               <Posts/>
             </Grid>
