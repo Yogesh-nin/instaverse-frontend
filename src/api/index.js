d6ea936b4be63325c632c2ab4dcf88aeb037a5bd
Yogesh Chandra <76822009+Yogesh-nin@users.noreply.github.com>
2024-07-01 20:09:25 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseUrl: 'https://localhost:5000'})
+const API = axios.create({ baseUrl: 'https://instaverse-r4ki.onrender.com'})
 
 API.interceptors.request.use((req)=>{
     if (localStorage.getItem('profile')){
